629fd6ec8e1e065513441bc75a1ede7885494081
Lâm Bảo Minh <baominh@MinMax.local>
2024-04-08 16:57:58 +0700

    addButton

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import "./App.css";
 import Header from "./layout/Header";
 import { AuthProvider } from "./Global/AuthContext";
+import Home from "./components/Home";
 
 class App extends Component {
     render() {
         return (
         <AuthProvider>
             <div className="App">
-                <Header></Header>
+                <Home/>
             </div>
         </AuthProvider>
         );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,16 +8,14 @@ function Home() {
     const { isLoggedIn } = useContext(AuthContext);
     return (
             <div className="Homepage">
-            {!isLoggedIn ? (
-                <Login/>
-            ) : (
                 <>
                     <h1>
                         <img src={"https://img.lovepik.com/element/45004/6953.png_860.png"} alt="Logo" style={{ width: "70px", height: "70px" }} />
                         DriveCare
                     </h1>
+                    <button type="button" className="manage">Manage</button>
                 </>
-            )}
+
             <Footer className="Footer"/>
             </div>
     );
