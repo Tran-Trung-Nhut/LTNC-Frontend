b7df725b62a4572604215c10fec2384817b843fa
PhuongN <thuphuong.nguyen.cntt@gmail.com>
2024-04-21 17:15:35 +0700

    [c] Rename Login.css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,52 +1,72 @@
 import React, { useContext } from "react";
-import { useState} from "react";
+import { useState } from "react";
 import axios from "axios";
 import AuthContext from "../Global/AuthContext";
 import Background from "../image/logo.jpg";
-import "./css/Login.css"
+import "./css/Login.css";
 
-function Login(){
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const { login, admin_password } = useContext(AuthContext);
-    
-    const handleLogin = (e) => {
-        e.preventDefault();
-        axios.get(`http://localhost:8000/Driver/login?password=${password}`)
-            .then(res => {
-                const driver = res.data;
-                if(password === admin_password && username === 'admin'){
-                    login('admin',admin_password,'admin');
-                }else{
-                    if(!driver){
-                        alert('Invalid username or password');
-                    }else{
-                        if(username === driver.name){
-                            login('user', password, username, driver);
-                        }else{
-                            alert('Invalid username or password');  
-                        }
-                    }
-                }
-            })
-    }
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { login, admin_password } = useContext(AuthContext);
 
-    return (
-    <div className="wrapper bg-cover bg-repeat-y z-2" style={{backgroundImage: `url(${Background})`}}>
-        <div className="login">Login
-            <div>
-                <input type="text" value={username} placeholder="Username" onChange={(e) => setUsername(e.target.value)}/>
-            </div>
-            <div>
-                <input type="password" value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
-            </div>
-            <div>
-                <button type="submit" className="btn btn-success" onClick={handleLogin}>Submit</button>
-            </div>
-        </div>   
-    </div>     
-    )
+  const handleLogin = (e) => {
+    e.preventDefault();
+    axios
+      .get(`http://localhost:8000/Driver/login?password=${password}`)
+      .then((res) => {
+        const driver = res.data;
+        if (password === "admin" && username === "admin") {
+          login("admin", "admin", "admin");
+        } else {
+          if (!driver) {
+            alert("Invalid username or password");
+          } else {
+            if (username === driver.name) {
+              login("user", password, username, driver);
+            } else {
+              alert("Invalid username or password");
+            }
+          }
+        }
+      });
+  };
+
+  return (
+    <div
+      className="wrapper bg-cover bg-repeat-y z-2"
+      style={{ backgroundImage: `url(${Background})` }}
+    >
+      <div className="login">
+        Login
+        <div>
+          <input
+            type="text"
+            value={username}
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <button
+            type="submit"
+            className="btn btn-success"
+            onClick={handleLogin}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Login
-
+export default Login;
